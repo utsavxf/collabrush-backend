3442897760582c98dc61d286afd8e2589a93c752
utsavxf <utb4578@gmail.com>
2024-06-24 13:45:32 +0530

    changed the endpoint

--- index.js
+++ index.js
@@ -6,8 +6,11 @@ const cors=require('cors');
 const app=express();
 const httpServer=createServer(app);
 
-app.use(cors({origin: 'http://localhost:3000'}));
-const io=new Server(httpServer,{cors:'http://localhost:3000'})
+const isDev=app.settings.env==='development';
+const URL=isDev?'http://localhost:5000':'https://collabrush-frontend-tb3r.vercel.app'
+
+app.use(cors({origin:URL}));
+const io=new Server(httpServer,{cors:URL})
 
 io.on('connection',(socket)=>{
   console.log('server connected');
